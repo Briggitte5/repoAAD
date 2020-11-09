6db2c6b73967d50e26b59d3267967e02d810ee1a
briggit <briggittearellano5@gmail.com>
2020-11-09 17:35:12 +0100

    subida a la nube

---
+++ UD1_E1_1_BriggitteArellano.java
@@ -0,0 +1,21 @@
+package UD1_E1_1_BriggitteArellano;
+
+public class UD1_E1_1_BriggitteArellano {
+
+	
+	public static void main(String[] args) {
+		//Inicio del main 
+		int i = -32768, i1 = -21692,  i2 = 16395;
+		    System.out.println("Números: "+i+" "+i1+" "+i2);
+			
+		    int total = i+ i1 + i2;
+		    System.out.print("Total de los números:");
+		    System.out.print( total );
+		    
+		    //Fin del main
+				
+				
+				
+				
+	}
+}
